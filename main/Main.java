5b8c2dd43e35f259045b54c2d8e548daeaabd3a2
JulienRabault <45000886+JulienRabault@users.noreply.github.com>
2019-04-11 14:28:11 +0200

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,90 @@
+package main;
+
+import graphe.Graphe;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Main {
+
+    private int posRobotPilote=-1;
+    private int posRobotIA=-1;
+
+    public Graphe inialisationGraphe(){
+
+        String nomConfig = "configuration/config1.txt";
+        File file = new File (nomConfig);
+        Map<Integer, List<Integer>> map = null;
+        List<Integer> victimes= null;
+        List<Integer> hopitaux=null;
+        Scanner scanner;
+        String current;
+        int nbSommet;
+
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+        while (scanner.hasNextLine()) {
+            current = scanner.nextLine();
+            switch (current) {
+
+                case ("S"):
+                    current = scanner.nextLine();
+                    nbSommet = Integer.parseInt(current);
+                    map = new LinkedHashMap<>();
+                    while (nbSommet > 0) {
+                        current = scanner.nextLine();
+                        String[] sommet = current.split(":");
+                        String[] voisin = sommet[1].split(",");
+                        map.put(Integer.parseInt(sommet[0]), construcListAvecString(voisin));
+                        nbSommet--;
+                    }
+                break;
+                case("V"):
+                    current = scanner.nextLine();
+                    victimes = construcListAvecString(current.split(","));
+                break;
+                case("H"):
+                    current = scanner.nextLine();
+                    hopitaux = construcListAvecString(current.split(","));
+                break;
+                case("RP"):
+                    current = scanner.nextLine();
+                    posRobotPilote = Integer.parseInt(current);
+                break;
+                case("RIA"):
+                    current = scanner.nextLine();
+                    posRobotIA = Integer.parseInt(current);
+                break;
+            }
+
+        }
+
+    return new Graphe(map,victimes,hopitaux);
+
+    }
+
+    private LinkedList<Integer> construcListAvecString (String[] voisinString){
+        LinkedList<Integer> voisin = new LinkedList<>();
+        for (String current : voisinString){
+            voisin.add(Integer.parseInt(current));
+        }
+        return voisin;
+    }
+
+    public int getPosRobotPilote() {
+        return posRobotPilote;
+    }
+
+    public int getPosRobotIA() {
+        return posRobotIA;
+    }
+}
+
+
