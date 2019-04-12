875bfb7af6abba12483b98d6a61b0588a93f0667
JulienRabault <45000886+JulienRabault@users.noreply.github.com>
2019-04-12 09:57:42 +0200

    Add files via upload

---
+++ New version/Code/Robot.java
@@ -0,0 +1,94 @@
+package Code;
+
+import Code.Graphe;
+
+public abstract class Robot {
+	private int currentPos;
+	private int previousPos;
+	private int initPos;
+	private Graphe graphe;
+
+	public Robot(int init, Graphe g) {
+		this.initPos = init;
+		this.currentPos = init;
+		this.previousPos = -1;
+		this.graphe = g;
+	}
+
+	public int getCurrentPos() {
+		return currentPos;
+	}
+
+	public int getPreviousPos() {
+		return previousPos;
+	}
+
+	public int getInitPos() {
+		return initPos;
+	}
+
+	public Graphe getGraphe() {
+		return graphe;
+	}
+
+	// construit le message � envoyer au robot
+	private String construct(boolean take, boolean drop, String action) {
+		String toSent = action;
+		if (!(take || drop)) {
+			toSent += "\n";
+			return "20" + toSent;
+		}
+		if (take) {
+			toSent += "t\n";
+		}
+		if (drop) {
+			toSent += "d\n";
+		}
+		return "30" + toSent;
+	}
+
+	// retourne la commande straight et mAj des positions apr�s d�placement
+	public String toutDroit(boolean take, boolean drop) {
+		int finalPos = graphe.toutDroit(previousPos, currentPos);
+		if (take) {
+			graphe.rammasserVictime(finalPos);
+		}
+		previousPos = currentPos;
+		currentPos = finalPos;
+		return construct(take, drop, "s");
+	}
+
+	// retourne la commande turn around et mAj des positions apr�s d�placement
+	public String demiTour(boolean take, boolean drop) {
+		int finalPos = previousPos;
+		if (take) {
+			graphe.rammasserVictime(previousPos);
+		}
+		previousPos = currentPos;
+		currentPos = finalPos;
+		return construct(take, drop, "u");
+	}
+
+	// retourne la commande right et mAj des positions apr�s d�placement
+	public String tournerDroite(boolean take, boolean drop) {
+		int finalPos = graphe.tournerDroite(previousPos, currentPos);
+		if (take) {
+			graphe.rammasserVictime(finalPos);
+		}
+		previousPos = currentPos;
+		currentPos = finalPos;
+		return construct(take, drop, "r");
+	}
+
+	// retourne la commande left et mAj des positions apr�s d�placement
+	public String tournerGauche(boolean take, boolean drop) {
+		int finalPos = graphe.tournerGauche(previousPos, currentPos);
+		if (take) {
+			graphe.rammasserVictime(finalPos);
+		}
+		previousPos = currentPos;
+		currentPos = finalPos;
+		return construct(take, drop, "l");
+	}
+
+}
