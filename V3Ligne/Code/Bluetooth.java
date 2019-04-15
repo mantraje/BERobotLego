761ebd5b1c98e6086aba464e90124b155a5c6e25
JulienRabault <45000886+JulienRabault@users.noreply.github.com>
2019-04-15 14:14:47 +0200

    Add files via upload

---
+++ V3Ligne/Code/Bluetooth.java
@@ -0,0 +1,41 @@
+package Code;
+
+
+
+import lejos.pc.comm.*;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Bluetooth {
+
+
+    /*public static void main (String args[]) {
+        //establish a connection...
+
+        NXTConnector com;
+        DataInputStream dataIn;
+        DataOutputStream dataOut;
+
+        boolean connected;
+
+        com = new NXTConnector();
+        connected = com.connectTo("Thorn","001653161388", NXTCommFactory.BLUETOOTH);
+        dataIn = new DataInputStream(com.getInputStream());
+        dataOut = new DataOutputStream(com.getOutputStream());
+
+    }/*
+
+    /*public static void main (String args[]) throws NXTCommException {
+
+
+        NXTComm comm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
+        NXTInfo nxtInfo = new NXTInfo(NXTCommFactory.BLUETOOTH, "Thorn", "001653161388");
+        comm.open(nxtInfo);
+
+
+    }*/
+
+
+}
